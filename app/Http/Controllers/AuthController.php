67b9b6d40695fc8564f131105fdf7e0178fa4969
fnocedapy <fnoceda@xhash.tech>
2023-06-14 11:46:51 -0400

    First commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        $validatedData = $request->validate([
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|max:255',
+        ]);
+        $user = User::create([
+            'first_name' => $validatedData['first_name'],
+            'last_name' => $validatedData['last_name'],
+            'email' => $validatedData['email'],
+            'password' => $validatedData['password'],
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+    public function login(Request $request){
+        $loginData = $request->validate([
+            'email' => 'required|max:255',
+            'password' => 'required',
+        ]);
+        if(!auth()->attempt($loginData)){
+            return response(['message' => 'Invalid Credentials']);
+        }
+        if(!Auth::attempt($request->only('email', 'password'))){
+            return response()->json(['message' => 'Invalid login details'], 401);
+        }
+        $user = User::where('email', $request['email'])->firstOrFail();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+
+
+    }
+
+    public function infoUser(Request $request){
+        return $request->user();
+    }
+
+}
